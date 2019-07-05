aa00c03212fad441c7c0f27f6abab2ae601a929d
Tristan Robert <tristan.robert.44@gmail.com>
2019-07-05 17:27:22 +0200

    Init spring boot

---
+++ src/main/java/com/github/mtesmct/rieau/rieauapi/RieauApplication.java
@@ -0,0 +1,13 @@
+package com.github.mtesmct.rieau.rieauapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RieauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RieauApplication.class, args);
+	}
+
+}
