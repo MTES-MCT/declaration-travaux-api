df3ca32be03d16f9f4b406ba07ad2673f16ba8e7
Tristan Robert <tristan.robert.44@gmail.com>
2019-08-13 11:29:06 +0200

    Ajout certificat autosigne pour dev

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dserver.port=$SERVER_PORT -cp app:app/lib/* com.github.mtesmct.rieau.api.Application
+keytool -noprompt -importcert -alias rieau -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -file $SERVER_CERT
+java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dserver.port=$SERVER_PORT -Djavax.net.debug=all -cp app:app/lib/* com.github.mtesmct.rieau.api.Application
