b561f935086c0f313331e2c99351e33cca83aa80
Tristan Robert <tristan.robert.44@gmail.com>
2019-08-14 11:07:20 +0200

    Fix docker container

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -z "$SERVER_CERT" ]]; then
+if [[ ! -z "$SERVER_CERT" ]]; then
     keytool -noprompt -importcert -alias rieau -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -file $SERVER_CERT
 fi
 java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dserver.port=$SERVER_PORT -cp app:app/lib/* com.github.mtesmct.rieau.api.Application
